70a91f90ce3972f0ab6a748c195e8700ee8a4106
jemtca <30645648+jemtca@users.noreply.github.com>
2020-03-26 21:29:30 -0700

    Updates

---
+++ precache-manifest.4a2cbca9baddb02ab0c43ea2983c3fa6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59382d20d6c6040f20a4f0cb7d7b8bd7",
+    "url": "/droidgalaxy-redux/index.html"
+  },
+  {
+    "revision": "df7dbf30c28aae35ba5a",
+    "url": "/droidgalaxy-redux/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "2568d66eb25966bbc313",
+    "url": "/droidgalaxy-redux/static/css/main.d552d0f7.chunk.css"
+  },
+  {
+    "revision": "df7dbf30c28aae35ba5a",
+    "url": "/droidgalaxy-redux/static/js/2.0a5e073b.chunk.js"
+  },
+  {
+    "revision": "eb56b19eba63a53416f4fd778023529f",
+    "url": "/droidgalaxy-redux/static/js/2.0a5e073b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2568d66eb25966bbc313",
+    "url": "/droidgalaxy-redux/static/js/main.b8e742a4.chunk.js"
+  },
+  {
+    "revision": "aea90c05bc9b189b7110",
+    "url": "/droidgalaxy-redux/static/js/runtime-main.f071fa31.js"
+  },
+  {
+    "revision": "701046a5c3b814e9ceeb69dabcfa4a58",
+    "url": "/droidgalaxy-redux/static/media/Death Star.701046a5.woff"
+  }
+]);
