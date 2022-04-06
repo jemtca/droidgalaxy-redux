df73d62462bcf755d99ae0a2a35feabf33f7d1df
jemtca <30645648+jemtca@users.noreply.github.com>
2022-04-06 12:40:01 -0700

    dependencies updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 // import { createLogger } from 'redux-logger';
@@ -14,10 +14,12 @@ import { searchDroids, requestDroids } from './reducers';
 const rootReducers = combineReducers({ searchDroids, requestDroids });
 const store = createStore(rootReducers, applyMiddleware(thunkMiddleware));
 
-ReactDOM.render(
-				<Provider store={store}>
-					<App />
-				</Provider>, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(<Provider store={store}>
+				<App />
+			</Provider>);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
